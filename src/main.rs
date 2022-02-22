2b2e2bda7b871474980e5617975442c363b62850
Kristoff <finley@kristoff.codes>
2022-02-22 11:24:34 -0700

    Basic Triangle Drawing

--- src/main.rs
+++ src/main.rs
@@ -31,18 +31,16 @@ fn main() {
         let program = gl.create_program().expect("Cannot create program");
 
         let (vertex_shader_source, fragment_shader_source) = (
-            r#"const vec2 verts[4] = vec2[4](
-                vec2(0.5f, 0.5f),   // top right
-                vec2(0.5f, -0.5f),  // bottom right
-                vec2(-0.5f, -0.5f), // bottom left
-                vec2(-0.5f, 0.5f)   // top left
+            r#"const vec2 verts[3] = vec2[3](
+                vec2(0.5f, 1.0f),
+                vec2(0.0f, 0.0f),
+                vec2(1.0f, 0.0f)
             );
             out vec2 vert;
             void main() {
                 vert = verts[gl_VertexID];
-                gl_Position = vec4(vert, 0.4, 1.0);
+                gl_Position = vec4(vert - 0.5, 0.0, 1.0);
             }"#,
-
             r#"precision mediump float;
             in vec2 vert;
             out vec4 color;
@@ -50,20 +48,6 @@ fn main() {
                 color = vec4(1.0, 1.0, 1.0, 1.0);
             }"#,
         );
-
-        // ebo attempt
-        let indices: [u8; 6] = [
-            0, 1, 3,
-            1, 2, 3
-        ];
-
-        // try to mimic Brook's buffer addIndexbuffer method (change the indices)
-
-
-        let ebo = gl.create_buffer().expect("ebo cannot be created");
-        gl.bind_buffer(ELEMENT_ARRAY_BUFFER, Some(ebo));
-        // messing up here
-        gl.buffer_data_u8_slice(ELEMENT_ARRAY_BUFFER,&indices, STATIC_DRAW);
 
         let shader_sources = [
             (glow::VERTEX_SHADER, vertex_shader_source),
@@ -109,8 +93,7 @@ fn main() {
                 }
                 Event::RedrawRequested(_) => {
                     gl.clear(glow::COLOR_BUFFER_BIT);
-                    gl.draw_elements(TRIANGLES, 6, UNSIGNED_INT, 0);
-                    // gl.draw_arrays(glow::TRIANGLES, 0, 3);
+                    gl.draw_arrays(glow::TRIANGLES, 0, 3);
                     window.swap_buffers().unwrap();
                 }
                 Event::WindowEvent { ref event, .. } => match event {
@@ -127,7 +110,5 @@ fn main() {
                 _ => (),
             }
         });
-
-        fn addIn
     }
 }
